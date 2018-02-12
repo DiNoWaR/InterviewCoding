e1bf065e3c4e912d34fd4f393dbcafe31c6527a6
dvasilyev <dvasilyev@ooyala.com>
2018-02-12 18:36:50 +0300

    Adds reverse list

---
+++ src/main/java/com/denis/interview/reverselist/LinkedList.java
@@ -0,0 +1,37 @@
+package com.denis.interview.reverselist;
+
+class LinkedList {
+
+    static Node head;
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Function to reverse the linked list */
+    Node reverse(Node node) {
+
+        Node prev = null;
+        Node next = null;
+        Node current = node;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        node = prev;
+
+        return node;
+    }
+
+}
