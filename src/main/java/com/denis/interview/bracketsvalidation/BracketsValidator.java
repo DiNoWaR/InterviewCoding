e74a02e0cd6854ea145c9bae867af7ed2f4588c6
dvasilyev <dvasilyev@ooyala.com>
2018-02-12 18:08:40 +0300

    Adds bracket validator

---
+++ src/main/java/com/denis/interview/bracketsvalidation/BracketsValidator.java
@@ -0,0 +1,46 @@
+package com.denis.interview.bracketsvalidation;
+
+import java.util.Stack;
+
+public class BracketsValidator {
+
+    private String brackets;
+
+    public BracketsValidator(String s) {
+        brackets = s;
+    }
+
+    public boolean validate() {
+
+        boolean result = true;
+
+        Stack<Character> stack = new Stack<Character>();
+
+        char current, previous;
+
+        for (int i = 0; i < this.brackets.length(); i++) {
+            current = this.brackets.charAt(i);
+
+            if (current == '(' || current == '[' || current == '{') {
+                stack.push(current);
+            } else if (current == ')' || current == ']' || current == '}') {
+
+                if (stack.isEmpty()) {
+                    result = false;
+                } else {
+                    previous = stack.peek();
+                    if ((current == ')' && previous == '(') || (current == ']' && previous == '[') || (current == '}' && previous == '{')) {
+                        stack.pop();
+                    } else {
+                        result = false;
+                    }
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            result = false;
+        }
+        return result;
+    }
+
+}
