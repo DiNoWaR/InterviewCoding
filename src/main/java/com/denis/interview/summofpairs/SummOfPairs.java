c511a5163a8ebb61ae074a70b6c1088c85f510f9
dvasilyev <dvasilyev@ooyala.com>
2018-02-12 19:10:21 +0300

    Adds summ of pairs task

---
+++ src/main/java/com/denis/interview/summofpairs/SummOfPairs.java
@@ -0,0 +1,39 @@
+package com.denis.interview.summofpairs;
+
+import java.util.Arrays;
+
+public class SummOfPairs {
+
+    /**
+     * Counts how many pairs of elements can be summed in unsorted array to get target element
+     *
+     * @param array
+     * @param target
+     * @return numbers of pairs
+     */
+    public int countPairsOfElementsInArray(int[] array, int target) {
+
+        int count = 0;
+
+        Arrays.sort(array);
+        int targetIndex = Arrays.binarySearch(array, target);
+
+        if (targetIndex == -1) {
+            return 0;
+        }
+
+        for (int i = 0; i <= targetIndex; i++) {
+
+            int j = targetIndex;
+
+
+        }
+
+        return count;
+    }
+
+    private boolean checkSumm(int first, int second, int target) {
+        return first + second == target;
+    }
+
+}
