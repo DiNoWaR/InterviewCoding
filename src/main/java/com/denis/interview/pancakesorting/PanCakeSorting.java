da7afdb61abebf244b78450967c875f62990bbf2
dvasilyev <dvasilyev@ooyala.com>
2018-02-14 11:48:17 +0300

    Adds pan cake sorting

--- src/main/java/com/denis/interview/pancakesorting/PanCakeSorting.java
+++ src/main/java/com/denis/interview/pancakesorting/PanCakeSorting.java
@@ -56,9 +56,7 @@ public class PanCakeSorting {
     public static void main(String[] args) {
 
         int[] arr = {99, 3, 10, 2, 34, 120, 16, 21, 28, 1};
-
         panCakeSort(arr);
-
         System.out.println(Arrays.toString(arr));
     }
 
