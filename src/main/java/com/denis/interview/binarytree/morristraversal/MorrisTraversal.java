59fac47d2b174439c5b1826bd3f87b0904098608
dvasilyev <dvasilyev@ooyala.com>
2018-02-28 16:11:15 +0300

    Adds Morris traversal

---
+++ src/main/java/com/denis/interview/binarytree/morristraversal/MorrisTraversal.java
@@ -0,0 +1,71 @@
+package com.denis.interview.binarytree.morristraversal;
+
+/**
+ * Using Morris Traversal, we can traverse the tree without using stack and recursion.
+ * The idea of Morris Traversal is based on Threaded Binary Tree.
+ * In this traversal, we first create links to Inorder successor and print the data using these links,
+ * and finally revert the changes to restore original tree.
+ */
+class MorrisTraversal {
+
+    TNode root;
+
+    /* Function to traverse binary tree without recursion and
+       without stack */
+    void MorrisTraversal(TNode root) {
+        TNode current, pre;
+
+        if (root == null)
+            return;
+
+        current = root;
+        while (current != null) {
+            if (current.left == null) {
+                System.out.print(current.data + " ");
+                current = current.right;
+            } else {
+                /* Find the inorder predecessor of current */
+                pre = current.left;
+                while (pre.right != null && pre.right != current)
+                    pre = pre.right;
+
+                /* Make current as right child of its inorder predecessor */
+                if (pre.right == null) {
+                    pre.right = current;
+                    current = current.left;
+                }
+
+                 /* Revert the changes made in if part to restore the
+                    original tree i.e.,fix the right child of predecssor*/
+                else {
+                    pre.right = null;
+                    System.out.print(current.data + " ");
+                    current = current.right;
+                }   /* End of if condition pre->right == NULL */
+
+            } /* End of if condition current->left == NULL*/
+
+        } /* End of while */
+
+    }
+
+    public static void main(String args[]) {
+
+        /* Constructed binary tree is
+               1
+             /   \
+            2      3
+          /  \
+        4     5
+        */
+
+        MorrisTraversal tree = new MorrisTraversal();
+        tree.root = new TNode(1);
+        tree.root.left = new TNode(2);
+        tree.root.right = new TNode(3);
+        tree.root.left.left = new TNode(4);
+        tree.root.left.right = new TNode(5);
+
+        tree.MorrisTraversal(tree.root);
+    }
+}
