5f02c75af681b9776d6117245217c8bca1a7aa2f
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-13 00:07:26 +0300

    Fix merge task

--- src/main/java/com/denis/interview/mergearrays/MergeArrays.java
+++ src/main/java/com/denis/interview/mergearrays/MergeArrays.java
@@ -9,6 +9,14 @@ public class MergeArrays {
 
     public static int[] merge(int[] first, int[] second) {
 
+        int[] small = first;
+        int[] big = second;
+
+        if (first.length > second.length) {
+            small = second;
+            big = first;
+        }
+
         int[] merged = new int[first.length + second.length];
 
         int firstCurrentIndex = 0;
@@ -16,13 +24,13 @@ public class MergeArrays {
 
         for (int i = 0; i < merged.length; i++) {
 
-            if (firstCurrentIndex < first.length && first[firstCurrentIndex] < second[secondCurrentIndex]) {
-                merged[i] = first[firstCurrentIndex];
+            if (firstCurrentIndex < small.length && small[firstCurrentIndex] < big[secondCurrentIndex]) {
+                merged[i] = small[firstCurrentIndex];
                 firstCurrentIndex++;
             } else {
 
-                if (secondCurrentIndex < second.length) {
-                    merged[i] = second[secondCurrentIndex];
+                if (secondCurrentIndex < big.length) {
+                    merged[i] = big[secondCurrentIndex];
                     secondCurrentIndex++;
                 }
             }
@@ -33,13 +41,12 @@ public class MergeArrays {
 
     public static void main(String[] args) {
 
-        int[] first = {1, 5, 5, 72, 100, 300};
+        int[] first = {1, 5, 5, 56, 72, 96, 100, 300};
         int[] second = {5, 7, 30, 200, 300, 332, 600, 800};
 
         int[] merged = merge(first, second);
 
         System.out.println(Arrays.toString(merged));
-
     }
 
 }
