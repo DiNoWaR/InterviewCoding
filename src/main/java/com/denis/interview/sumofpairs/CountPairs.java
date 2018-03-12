f688596a96fef80e905a65eea75d5565b440d133
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-13 00:14:58 +0300

    Fix merge task

---
+++ src/main/java/com/denis/interview/sumofpairs/CountPairs.java
@@ -0,0 +1,21 @@
+package com.denis.interview.sumofpairs;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+public class CountPairs {
+
+
+    public int countPairsOfElementsInArray(int[] array, int target) {
+
+        int count = 0;
+
+        Arrays.sort(array);
+
+        return count;
+    }
+
+
+}
