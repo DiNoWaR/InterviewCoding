390c877e01a8b761a3e31bf36bd7c8af81e3d98b
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-13 00:42:22 +0300

    Fix pairs task

--- src/main/java/com/denis/interview/sumofpairs/CountPairs.java
+++ src/main/java/com/denis/interview/sumofpairs/CountPairs.java
@@ -8,14 +8,31 @@ import java.util.Arrays;
 public class CountPairs {
 
 
-    public int countPairsOfElementsInArray(int[] array, int target) {
+    public static int countPairsOfElementsInArray(int[] array, int target) {
 
         int count = 0;
 
         Arrays.sort(array);
 
-        return count;
+        for (int item : array) {
+
+            int search = target - item;
+            int elementIndex = Arrays.binarySearch(array, search);
+
+            if (elementIndex >= 0) {
+                count++;
+            }
+        }
+
+        return count / 2;
     }
 
+    public static void main(String[] args) {
+
+        int[] p = {2, 3, 5, 8, 6, 0};
+
+        System.out.println(countPairsOfElementsInArray(p, 8));
+
+    }
 
 }
