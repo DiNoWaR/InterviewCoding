c8e34f3a2d39ed9bd84b483129aa515dd408769d
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-17 22:15:35 +0300

    Adds findStarElement

--- src/main/java/com/denis/interview/algorithms/starelements/StarElements.java
+++ src/main/java/com/denis/interview/algorithms/starelements/StarElements.java
@@ -1,6 +1,8 @@
 package com.denis.interview.algorithms.starelements;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Given an unsorted array. The task is to find all the star and super star elements in the array.
@@ -9,7 +11,24 @@ import java.util.Arrays;
  */
 public class StarElements {
 
+    private static List<Integer> findStar(int[] arr) {
+
+        List<Integer> starList = new ArrayList<>();
+
+        int star = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i >= 1; i--) {
+
+            if (star < arr[i - 1]) {
+                starList.add(arr[i - 1]);
+                star = arr[i - 1];
+            }
+        }
+        return starList;
+    }
+
     private static int findSuperStar(int[] arr) {
+
         int superStar = arr[0];
 
         for (int i = 0; i < arr.length - 1; i++) {
@@ -20,14 +39,16 @@ public class StarElements {
         if (!isSuperStarUnique(arr, superStar)) {
             return -1;
         }
-
         return superStar;
     }
 
     private static boolean isSuperStarUnique(int[] arr, int superStar) {
+
         int[] arr2 = new int[arr.length];
+
         System.arraycopy(arr, 0, arr2, 0, arr.length);
         Arrays.sort(arr2);
+
         if (arr2[arr2.length - 1] == arr2[arr2.length - 2]) {
             return false;
         }
@@ -35,10 +56,14 @@ public class StarElements {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,9,3,5,11,4334,545,4334,1};
-        int[] arr2 = {1,9,3,5,11,4334,545,1};
-        System.out.println(findSuperStar(arr));
-        System.out.println(findSuperStar(arr2));
+        int[] arr = {1, 9, 3, 5, 11, 4334, 545, 4334, 1};
+        int[] arr2 = {1, 9, 3, 5, 11, 4334, 545, 1};
+
+        System.out.println(findStar(arr));
+        System.out.println(findStar(arr2));
+
+//        System.out.println(findSuperStar(arr));
+//        System.out.println(findSuperStar(arr2));
     }
 }
 
