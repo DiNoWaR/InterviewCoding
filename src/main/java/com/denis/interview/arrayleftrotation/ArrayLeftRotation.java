0efd7959d799a76e68891697c984953669fc1816
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-17 18:34:51 +0300

    Adds 2 tasks

--- src/main/java/com/denis/interview/arrayleftrotation/ArrayLeftRotation.java
+++ src/main/java/com/denis/interview/arrayleftrotation/ArrayLeftRotation.java
@@ -35,32 +35,4 @@ import java.util.Scanner;
  */
 public class ArrayLeftRotation {
 
-
-    public static int[] arrayLeftRotation(int[] a, int n, int k) {
-        return null;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-
-        int n = in.nextInt();
-        int k = in.nextInt();
-
-        int[] a = new int[n];
-
-        for (int a_i = 0; a_i < n; a_i++) {
-            a[a_i] = in.nextInt();
-        }
-
-        int[] output = arrayLeftRotation(a, n, k);
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(output[i] + " ");
-        }
-
-        System.out.println();
-    }
-
-
 }
