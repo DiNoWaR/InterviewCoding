fd4966f0369be97a316384bada27de74085994c9
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-17 18:39:56 +0300

    Changes structure

---
+++ src/main/java/com/denis/interview/algorithms/binarytree/morristraversal/TNode.java
@@ -0,0 +1,12 @@
+package com.denis.interview.algorithms.binarytree.morristraversal;
+
+public class TNode {
+
+    int data;
+    TNode left, right;
+
+    TNode(int item) {
+        data = item;
+        left = right = null;
+    }
+}
