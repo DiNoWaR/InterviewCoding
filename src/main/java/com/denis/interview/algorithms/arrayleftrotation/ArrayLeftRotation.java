6e8088593ba71648d7d0142e97c8ff0f31cd5f06
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-18 01:07:19 +0300

    Adds pancakeSortWithBurntSide task

--- src/main/java/com/denis/interview/algorithms/arrayleftrotation/ArrayLeftRotation.java
+++ src/main/java/com/denis/interview/algorithms/arrayleftrotation/ArrayLeftRotation.java
@@ -7,17 +7,6 @@ package com.denis.interview.algorithms.arrayleftrotation;
  * Given an array of  integers and a number, , perform  left rotations on the array.
  * Then print the updated array as a single line of space-separated integers.
  * <p>
- * Input Format
- * <p>
- * The first line contains two space-separated integers denoting the respective values of  (the number of integers) and  (the number of left rotations you must perform).
- * The second line contains  space-separated integers describing the respective elements of the array's initial state.
- * <p>
- * Constraints
- * <p>
- * Output Format
- * <p>
- * Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
- * <p>
  * Sample Input
  * <p>
  * 5 4
