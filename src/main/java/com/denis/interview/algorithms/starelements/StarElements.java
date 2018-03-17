d5ad71b62ace12dd5bfe1bee8e3e81e019013c42
bugaved <bugaved@gmail.com>
2018-03-17 21:58:41 +0300

    Added findSuperStarElement to StarElements

--- src/main/java/com/denis/interview/algorithms/starelements/StarElements.java
+++ src/main/java/com/denis/interview/algorithms/starelements/StarElements.java
@@ -1,5 +1,7 @@
 package com.denis.interview.algorithms.starelements;
 
+import java.util.Arrays;
+
 /**
  * Given an unsorted array. The task is to find all the star and super star elements in the array.
  * Star are those elements which are strictly greater than all the elements on its right side.
@@ -7,4 +9,36 @@ package com.denis.interview.algorithms.starelements;
  */
 public class StarElements {
 
+    private static int findSuperStar(int[] arr) {
+        int superStar = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
+                superStar = arr[i + 1];
+            }
+        }
+        if (!isSuperStarUnique(arr, superStar)) {
+            return -1;
+        }
+
+        return superStar;
+    }
+
+    private static boolean isSuperStarUnique(int[] arr, int superStar) {
+        int[] arr2 = new int[arr.length];
+        System.arraycopy(arr, 0, arr2, 0, arr.length);
+        Arrays.sort(arr2);
+        if (arr2[arr2.length - 1] == arr2[arr2.length - 2]) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,9,3,5,11,4334,545,4334,1};
+        int[] arr2 = {1,9,3,5,11,4334,545,1};
+        System.out.println(findSuperStar(arr));
+        System.out.println(findSuperStar(arr2));
+    }
 }
+
