08b2146bd4122d4b21a36751bcbf1830560e10c5
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-17 18:32:05 +0300

    Adds 2 tasks

--- src/main/java/com/denis/interview/ironman/IronMan.java
+++ src/main/java/com/denis/interview/ironman/IronMan.java
@@ -1,7 +1,12 @@
 package com.denis.interview.ironman;
 
 /**
+ * IronMan is ready for battle. He starts his battle at index 0, moves in 1 index on the array each iteration.
+ * Each index of array contains the number.If it's positive, charge of IronMan increases by number.Otherwise descrease by number.
+ * If the charge of IronMan is less than 1, he dies.
+ * Find minimum charge of IronMan to go through array and stay alive.
  *
+ * Note: Array may be huge (10^5)
  */
 public class IronMan {
 
@@ -29,7 +34,7 @@ public class IronMan {
 
     public static void main(String[] args) {
 
-        int[] p = {-5, 4, -19, 3, 1, -1, -6, -1, 0, 5, -13};
+        int[] p = {-5, 4, -2, 3, 1, -1, -6, -1, 0, -5};
 
         System.out.println(ironMan(p));
 
