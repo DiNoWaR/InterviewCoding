0777abc66888a0688ab964864e45d3626baaa20a
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-18 21:56:10 +0300

    Changes Morris Traversal

--- src/main/java/com/denis/interview/util/TreeNode.java
+++ src/main/java/com/denis/interview/util/TreeNode.java
@@ -15,12 +15,9 @@ public class TreeNode<T> {
         this.value = value;
     }
 
+
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 
     public TreeNode<T> getLeft() {
