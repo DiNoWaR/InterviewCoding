55e8e59e03478ab2f3974cb98813d164a18c2e57
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-18 23:23:53 +0300

    Adds Tree generator

--- src/main/java/com/denis/interview/algorithms/binarytrees/traversal/morristraversal/MorrisTraversal.java
+++ src/main/java/com/denis/interview/algorithms/binarytrees/traversal/morristraversal/MorrisTraversal.java
@@ -1,5 +1,6 @@
 package com.denis.interview.algorithms.binarytrees.traversal.morristraversal;
 
+import com.denis.interview.util.BinaryTreeGenerator;
 import com.denis.interview.util.TreeNode;
 
 /**
@@ -58,27 +59,7 @@ class MorrisTraversal {
 
     public static void main(String args[]) {
 
-        TreeNode<Integer> root = new TreeNode<>(8);
-
-        TreeNode<Integer> first = new TreeNode<>(3);
-        TreeNode<Integer> second = new TreeNode<>(10);
-        TreeNode<Integer> third = new TreeNode<>(1);
-        TreeNode<Integer> fourth = new TreeNode<>(6);
-        TreeNode<Integer> fifth = new TreeNode<>(4);
-        TreeNode<Integer> sixth = new TreeNode<>(7);
-        TreeNode<Integer> seventh = new TreeNode<>(13);
-        TreeNode<Integer> eights = new TreeNode<>(14);
-
-        root.setLeft(first);
-        root.setRight(second);
-        first.setLeft(third);
-        first.setRight(fourth);
-        fourth.setLeft(fifth);
-        fourth.setRight(sixth);
-        second.setRight(eights);
-        eights.setLeft(seventh);
-
-
+        TreeNode<Integer> root = BinaryTreeGenerator.generateBST();
         morrisTraversal(root);
     }
 }
