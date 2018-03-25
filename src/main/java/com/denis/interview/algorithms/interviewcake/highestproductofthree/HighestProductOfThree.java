8c67c6c52e38cb994e84d5fc2144e8487c11ab4b
DiNoWaR <Denis_Vasilyev1@epam.com>
2018-03-25 18:12:16 +0300

    Adds highest of 3 etalon solution

--- src/main/java/com/denis/interview/algorithms/interviewcake/highestproductofthree/HighestProductOfThree.java
+++ src/main/java/com/denis/interview/algorithms/interviewcake/highestproductofthree/HighestProductOfThree.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class HighestProductOfThree {
 
-    public static int getMaxProductOfThreeWithSorting(int[] array) {
+    public static int getMaxProductOf3WithSorting(int[] array) {
 
         Arrays.sort(array);
 
@@ -22,7 +22,7 @@ public class HighestProductOfThree {
         return result;
     }
 
-    public static int getMaxProductOfThree(int[] array) {
+    public static int getMaxProductOf3OurSolution(int[] array) {
 
         if (array.length == 3) {
             return array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
@@ -67,13 +67,59 @@ public class HighestProductOfThree {
         return max * min * min2;
     }
 
+    public static int getMaxProductOf3EtalonSolution(int[] array) {
+
+        if (array.length < 3) {
+            throw new IllegalArgumentException("Less than 3 items!");
+        }
+
+        // we're going to start at the 3rd item (at index 2)
+        // so pre-populate highests and lowests based on the first 2 items.
+        // we could also start these as null and check below if they're set
+        // but this is arguably cleaner
+        int highest = Math.max(array[0], array[1]);
+        int lowest = Math.min(array[0], array[1]);
+
+        int highestProductOf2 = array[0] * array[1];
+        int lowestProductOf2 = array[0] * array[1];
+
+        // except this one--we pre-populate it for the first *3* items.
+        // this means in our first pass it'll check against itself, which is fine.
+        int highestProductOf3 = array[0] * array[1] * array[2];
+
+        // walk through items, starting at index 2
+        for (int i = 2; i < array.length; i++) {
+            int current = array[i];
+
+            // do we have a new highest product of 3?
+            // it's either the current highest,
+            // or the current times the highest product of two
+            // or the current times the lowest product of two
+            highestProductOf3 = Math.max(Math.max(highestProductOf3, current * highestProductOf2), current * lowestProductOf2);
+
+            // do we have a new highest product of two?
+            highestProductOf2 = Math.max(Math.max(highestProductOf2, current * highest), current * lowest);
+
+            // do we have a new lowest product of two?
+            lowestProductOf2 = Math.min(Math.min(lowestProductOf2, current * highest), current * lowest);
+
+            // do we have a new highest?
+            highest = Math.max(highest, current);
+
+            // do we have a new lowest?
+            lowest = Math.min(lowest, current);
+        }
+
+        return highestProductOf3;
+    }
+
     public static void main(String[] args) {
 
         int[] array = {-1, -1000, -1, -5, -90};
         int[] array2 = {1, 100, 5, 100, 100};
 
-        System.out.println(getMaxProductOfThree(array));
-        System.out.println(getMaxProductOfThree(array2));
+        System.out.println(getMaxProductOf3OurSolution(array));
+        System.out.println(getMaxProductOf3OurSolution(array2));
 
     }
 }
