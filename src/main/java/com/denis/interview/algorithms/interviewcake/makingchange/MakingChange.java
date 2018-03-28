0fba9286ebccd7068c4aef3802489a7352cce4e3
DiNoWaR <unreal2007>
2018-03-28 23:49:32 +0300

    Adds Making change task

--- src/main/java/com/denis/interview/algorithms/interviewcake/makingchange/MakingChange.java
+++ src/main/java/com/denis/interview/algorithms/interviewcake/makingchange/MakingChange.java
@@ -11,7 +11,7 @@ package com.denis.interview.algorithms.interviewcake.makingchange;
  * an array of coin denominations
  * computes the number of ways to make the amount of money with coins of the available denominations.
  *
- * Example: for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢), your program would output 44—the number of ways to make 44¢ with those denominations:
+ * Example: for amount=4 (4¢) and denominations=[1,2,3](1¢, 2¢ and 3¢), your program would output 4—the number of ways to make 4¢ with those denominations:
  *
  * 1¢, 1¢, 1¢, 1¢
  * 1¢, 1¢, 2¢
