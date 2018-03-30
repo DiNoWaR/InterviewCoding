5daa85ff007e1ae15b88c2f385d1ef9eb32048cc
DiNoWaR <unreal2007>
2018-03-30 22:16:11 +0300

    Adds fix to left shift task

--- src/main/java/com/denis/interview/algorithms/arrays/arrayleftrotation/ArrayLeftRotation.java
+++ src/main/java/com/denis/interview/algorithms/arrays/arrayleftrotation/ArrayLeftRotation.java
@@ -8,8 +8,7 @@ import java.util.Arrays;
  * Given an array of  integers and a number, , perform  left rotations on the array.
  * Then print the updated array as a single line of space-separated integers.
  * Sample Input
- * 5 4
- * 1 2 3 4 5
+ * 1 2 3 4 5 and shift 4
  * Sample Output
  * 5 1 2 3 4
  * Explanation
