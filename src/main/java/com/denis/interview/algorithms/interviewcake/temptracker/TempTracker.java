eb6e989d8d3b58e34bcdea4c94fda7eb355b54dc
DiNoWaR <unreal2007>
2018-03-30 23:49:59 +0300

    Adds some fix

--- src/main/java/com/denis/interview/algorithms/interviewcake/temptracker/TempTracker.java
+++ src/main/java/com/denis/interview/algorithms/interviewcake/temptracker/TempTracker.java
@@ -6,13 +6,13 @@ import java.util.List;
 /**
  * You decide to test if your oddly-mathematical heating company is fulfilling its All-Time Max, Min, Mean and Mode Temperature Guarantee™.
  * Write a class TempTracker with these methods:
- *
+ * <p>
  * insert()—records a new temperature
  * getMax()—returns the highest temp we've seen so far
  * getMin()—returns the lowest temp we've seen so far
  * getMean()—returns the mean ↴ of all temps we've seen so far
  * getMode()—returns a mode ↴ of all temps we've seen so far
- *
+ * <p>
  * Optimize for space and time.
  * Favor speeding up the getter methods getMax(), getMin(), getMean(), and getMode() over speeding up the insert() method.
  * getMean() should return a double, but the rest of the getter methods can return integers. Temperatures will all be inserted as integers.
