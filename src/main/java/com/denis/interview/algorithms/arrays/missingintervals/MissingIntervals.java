4876723c3a236372c51509e284418e948203a65a
dvasilyev <dvasilyev@ooyala.com>
2018-04-02 19:17:39 +0300

    Adds missing intervals task

--- src/main/java/com/denis/interview/algorithms/arrays/missingintervals/MissingIntervals.java
+++ src/main/java/com/denis/interview/algorithms/arrays/missingintervals/MissingIntervals.java
@@ -1,5 +1,8 @@
 package com.denis.interview.algorithms.arrays.missingintervals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given a sorted array A of integers  from 0 to 99 (repetition of elements allowed),
  * output all the integer intervals missing from the given list.
@@ -18,5 +21,73 @@ package com.denis.interview.algorithms.arrays.missingintervals;
  * <p>
  */
 public class MissingIntervals {
+
+    private final static int MAX = 99;
+
+    public static List<String> getIntervals(int[] array) {
+
+        List<String> intervals = new ArrayList<>();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            String interval;
+
+            if (i == 0) {
+
+                if (array[i] > 0) {
+
+                    if (array[i] == 1) {
+                        interval = "0";
+                        intervals.add(interval);
+                    } else {
+                        interval = buildInterval(-1, array[i]);
+                        intervals.add(interval);
+                    }
+                }
+            }
+
+            interval = buildInterval(array[i], array[i + 1]);
+
+            if (!"".equals(interval)) {
+                intervals.add(interval);
+            }
+
+            if (i == array.length - 2) {
+                if (array[i + 1] == MAX) {
+
+                } else if (array[i + 1] == MAX - 1) {
+                    intervals.add(Integer.toString(MAX));
+                } else {
+                    interval = buildInterval(array[i + 1], MAX + 1);
+                    intervals.add(interval);
+                }
+            }
+        }
+        return intervals;
+    }
+
+    private static String buildInterval(int from, int to) {
+
+        if ((to - from) > 2) {
+            return Integer.toString(from + 1) + "-" + Integer.toString(to - 1);
+        }
+
+        if ((to - from) == 2) {
+            return Integer.toString(from + 1);
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 5, 67, 88};
+        int[] array2 = {4, 55, 77, 89, 91, 99};
+        int[] array3 = {0, 3, 4, 6, 80, 90, 91};
+
+        System.out.println(getIntervals(array1));
+        System.out.println(getIntervals(array2));
+        System.out.println(getIntervals(array3));
+
+    }
 
 }
