210ea9688eb07c9ab237d5576cdf8e20affada4a
DiNoWaR <unreal2007>
2018-04-05 00:07:06 +0300

    Adds some fixes for java 10

--- src/main/java/com/denis/interview/algorithms/interviewcake/makingchange/MakingChange.java
+++ src/main/java/com/denis/interview/algorithms/interviewcake/makingchange/MakingChange.java
@@ -21,12 +21,14 @@ package com.denis.interview.algorithms.interviewcake.makingchange;
 public class MakingChange {
 
     public static int changePossibilitiesBottomUp(int amount, int[] denominations) {
-        int[] waysOfDoingNCents = new int[amount + 1];  // array of zeros from 0..amount
+
+        var waysOfDoingNCents = new int[amount + 1];  // array of zeros from 0..amount
+
         waysOfDoingNCents[0] = 1;
 
-        for (int coin : denominations) {
-            for (int higherAmount = coin; higherAmount <= amount; higherAmount++) {
-                int higherAmountRemainder = higherAmount - coin;
+        for (var coin : denominations) {
+            for (var higherAmount = coin; higherAmount <= amount; higherAmount++) {
+                var higherAmountRemainder = higherAmount - coin;
                 waysOfDoingNCents[higherAmount] += waysOfDoingNCents[higherAmountRemainder];
             }
         }
