dae6ffdcd9055f62c1bee4d1666223a65b278501
DiNoWaR <unreal2007>
2018-05-09 18:49:55 +0300

    Adds sort stack

--- src/main/java/com/denis/interview/algorithms/stacks/sortstack/SortStack.java
+++ src/main/java/com/denis/interview/algorithms/stacks/sortstack/SortStack.java
@@ -3,81 +3,31 @@ package com.denis.interview.algorithms.stacks.sortstack;
 import java.util.Stack;
 
 public class SortStack {
-    private Stack<Integer> buffer;
-    private Integer min;
-    private int counter;
 
-    public SortStack() {
-        buffer = new Stack<>();
-    }
+    public static Stack<Integer> sortstack(Stack<Integer> input) {
 
-    public int getCounter() {
-        return counter;
-    }
+        Stack<Integer> tmpStack = new Stack<Integer>();
 
-    public void sort(Stack<Integer> source) {
-        while (!source.isEmpty()) {
-            if (min == null) {
-                min = source.pop();
-            } else if (min >= source.peek()) {
-                buffer.push(min);
-                counter++;
-                min = source.pop();
-            } else {
-                buffer.push(source.pop());
-                counter++;
+        while (!input.isEmpty()) {
+            // pop out the first element
+            int tmp = input.pop();
+
+            // while temporary stack is not empty and
+            // top of stack is greater than temp
+            while (!tmpStack.isEmpty() && tmpStack.peek()
+                    > tmp) {
+                // pop from temporary stack and
+                // push it to the input stack
+                input.push(tmpStack.pop());
             }
+
+            // push temp in tempory of stack
+            tmpStack.push(tmp);
         }
-        if (counter == 0) {
-            buffer.push(min);
-            while (!buffer.isEmpty()) {
-                source.push(buffer.pop());
-            }
-            return;
-        }
-        if (counter > 0) {
-            move(buffer, source, counter);
-            sort(source);
-        }
-
-    }
-
-    private void move(Stack<Integer> src, Stack<Integer> dest, int counter) {
-        while (counter > 0) {
-            dest.push(src.pop());
-            counter--;
-        }
-        this.counter = 0;
-        src.push(this.min);
-        this.min = null;
-    }
-
-    public Integer getMin() {
-        return min;
-    }
-
-    public Stack<Integer> getBuffer() {
-
-        return buffer;
+        return tmpStack;
     }
 
     public static void main(String[] args) {
-        Stack<Integer> source = new Stack<>();
-        source.push(3);
-        source.push(3);
-        source.push(3);
-        source.push(2);
-        source.push(17);
-        source.push(41);
-        source.push(500);
 
-        SortStack sortStack = new SortStack();
-        sortStack.sort(source);
-        System.out.println(sortStack.getMin());
-        System.out.println(source.isEmpty());
-        System.out.println(sortStack.getCounter());
-        while (!source.isEmpty()) {
-            System.out.println(source.pop());
-        }
     }
 }
