c26a3baa79ef28a702005de99cbc9c612bd16f2c
bugaved <bugaved@gmail.com>
2018-05-11 23:58:06 +0300

    OneWay

--- src/main/java/com/denis/interview/algorithms/other/oneway/OneWay.java
+++ src/main/java/com/denis/interview/algorithms/other/oneway/OneWay.java
@@ -13,6 +13,60 @@ public class OneWay {
         }
 
 
+        if (src.length() == target.length()) {
+            int counter = 0;
+            for (int i = 0; i < src.length(); i++) {
+                if (src.charAt(i) != target.charAt(i)) {
+                    counter++;
+                }
+            }
+            if (counter == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        String longString = src;
+        String shortString = target;
+
+        if (src.length() < target.length()) {
+            longString = target;
+            shortString = src;
+        }
+        int k = 0;
+        int counter = 0;
+        for (int i = 0; i < longString.length(); i++) {
+            if (counter == 0 && i == longString.length() - 1) {
+                return true;
+            }
+            if (src.charAt(i) != shortString.charAt(k)) {
+                counter++;
+            } else {
+                k++;
+            }
+        }
+        if (counter < 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public static void main(String[] args) {
+        OneWay oneWay = new OneWay();
+        System.out.println(oneWay.checkString("bugaga", "bugag"));
+        System.out.println(oneWay.checkString("bugaga", "bugaga"));
+        System.out.println(oneWay.checkString("bugaga", "bugapa"));
+        System.out.println(oneWay.checkString("bugaga", "buaga"));
+        System.out.println(oneWay.checkString("bugaga", "ugaga"));
+        System.out.println(oneWay.checkString("bugaga", "bumaga"));
+        System.out.println(oneWay.checkString("", "1"));
+        System.out.println(oneWay.checkString("1", ""));
+        System.out.println("-----------------------------------------------------");
+        System.out.println(oneWay.checkString("bugaga", "bugggg"));
+        System.out.println(oneWay.checkString("bugaga", "alalot"));
+        System.out.println(oneWay.checkString("bugaga", "bugggg"));
+        System.out.println(oneWay.checkString("bugaga", "jkgfdslgj"));
+        System.out.println(oneWay.checkString("bugaga", "abugag"));
+    }
 }
