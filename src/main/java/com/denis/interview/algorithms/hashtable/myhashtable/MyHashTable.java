965f799030ca8278f26ee18773d1b5768e92ceb5
bugaved <bugaved@gmail.com>
2018-05-13 21:56:29 +0300

    HashTable

--- src/main/java/com/denis/interview/algorithms/hashtable/myhashtable/MyHashTable.java
+++ src/main/java/com/denis/interview/algorithms/hashtable/myhashtable/MyHashTable.java
@@ -9,6 +9,7 @@ public class MyHashTable<K, V> {
     private LinkedList<Entry<K, V>>[] keyArray;
     private double loadFactor = 0.7;
     private int initialCapacity = 16;
+    private int size = 0;
 
     public MyHashTable() {
         keyArray = new LinkedList[initialCapacity];
@@ -18,14 +19,27 @@ public class MyHashTable<K, V> {
     public void put(K key, V value) {
 
         int index = hashFunction(key);
-
+        if (keyArray.length * loadFactor < size) {
+            rehash();
+        }
         if (keyArray[index] == null) {
             LinkedList<Entry<K, V>> list = new LinkedList<>();
             list.add(new Entry<>(key, value));
             keyArray[index] = list;
+            size++;
         } else {
-            keyArray[index].add(new Entry<>(key, value));
+            boolean exist = false;
+            for (Entry<K, V> entry : keyArray[index]) {
+                if (entry.key.equals(key)) {
+                    entry.value = value;
+                    exist = true;
+                }
+            }
+            if (!exist) {
+                keyArray[index].add(new Entry<>(key, value));
+            }
         }
+
     }
 
     public V get(K key) {
@@ -43,23 +57,65 @@ public class MyHashTable<K, V> {
         }
         return null;
     }
+    public void remove(K key){
+        int index = hashFunction(key);
+        if(keyArray[index]!=null){
+            for(Entry<K,V> entry:keyArray[index]){
+                if(entry.key.equals(key)){
+                    keyArray[index].remove(entry);
+                }
+            }
+        }
+    }
 
     private int hashFunction(K key) {
         return key.hashCode() % keyArray.length;
     }
 
     private void rehash() {
-        List<Entry<K, V>>[] newArray = new List[keyArray.length * 2];
+        LinkedList<Entry<K, V>>[] newArray = new LinkedList[keyArray.length * 2];
 
         for (int i = 0; i < keyArray.length; i++) {
-            int newIndex = hashFunction(keyArray[i].getFirst().key);
-
+            if (keyArray[i] != null) {
+                for (Entry<K, V> entry : keyArray[i]) {
+                    int index = hashFunction(entry.key);
+                    if (newArray[index] == null) {
+                        LinkedList<Entry<K, V>> list = new LinkedList<>();
+                        list.add(entry);
+                        newArray[index] = list;
+                    } else {
+                        newArray[index].add(entry);
+                    }
+                }
+            }
         }
+        keyArray = newArray;
 
     }
 
     public static void main(String[] args) {
-
+        MyHashTable<String, Integer> myHashTable = new MyHashTable<>();
+        myHashTable.put("bgg", 1);
+        myHashTable.put("bgg2", 2);
+        myHashTable.put("bgg3", 3);
+        myHashTable.put("bgg4", 4);
+        myHashTable.put("bgg5", 5);
+        myHashTable.put("bgg6", 6);
+        myHashTable.put("bgg7", 7);
+        myHashTable.put("bgg8", 8);
+        myHashTable.put("bgg9", 9);
+        myHashTable.put("bgg10", 10);
+        myHashTable.remove("bgg4");
+        System.out.println(myHashTable.get("bgg"));
+        System.out.println(myHashTable.get("bgg2"));
+        System.out.println(myHashTable.get("bgg3"));
+        System.out.println(myHashTable.get("bgg4"));
+        System.out.println(myHashTable.get("bgg5"));
+        System.out.println(myHashTable.get("bgg6"));
+        System.out.println(myHashTable.get("bgg7"));
+        System.out.println(myHashTable.get("bgg8"));
+        System.out.println(myHashTable.get("bgg9"));
+        System.out.println(myHashTable.get("bgg10"));
     }
 
     static class Entry<K, V> {
