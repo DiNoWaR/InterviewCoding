b644ac61be931dab5ab8e456886e5b7b3197c897
DiNoWaR <unreal2007>
2018-05-22 21:50:10 +0300

    Adds set of stacks

--- src/main/java/com/denis/interview/algorithms/stacks/setofstacks/SetOfStacks.java
+++ src/main/java/com/denis/interview/algorithms/stacks/setofstacks/SetOfStacks.java
@@ -5,6 +5,15 @@ import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+ * Therefore,in real life, we would likely start a new stack when the previous stack exceeds some threshold.
+ * Implement a data structure SetOfStacks that mimics this. SetOf-Stacks should be composed of several stacks
+ * and should create a new stack once the previous one exceeds capacity.
+ * SetOfStacks.push() and SetOfStacks.pop () should behave identically to a single stack (that is, pop () should return the same values as it would if there were just a single stack).
+ *
+ * Implement a function popAt(int index) which performs a pop operation on a specific sub-stack.
+ */
 public class SetOfStacks {
 
     private List<Stack<Integer>> list;
