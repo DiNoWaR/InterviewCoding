c2ec7af178b1d592c830b600b88c3fa38b0cf812
DiNoWaR <unreal2007>
2018-05-22 21:42:01 +0300

    Adds set of stacks

--- src/main/java/com/denis/interview/algorithms/stacks/setofstacks/SetOfStacks.java
+++ src/main/java/com/denis/interview/algorithms/stacks/setofstacks/SetOfStacks.java
@@ -8,18 +8,21 @@ import java.util.Stack;
 public class SetOfStacks {
 
     private List<Stack<Integer>> list;
-    private int currentStackSize = 0;
 
+    private int currentStackSize = 0;
     private int currentStackIndex = 0;
 
     private final int maxSize;
 
+    private Stack<Integer> buffer;
 
     public SetOfStacks(int maxSize) {
         list = new ArrayList<>();
+        buffer = new Stack<>();
         addStackToList();
         this.maxSize = maxSize;
     }
+
 
     public void push(Integer item) {
 
@@ -51,7 +54,7 @@ public class SetOfStacks {
         if (list.get(currentStackIndex).isEmpty()) {
             list.remove(currentStackIndex);
             currentStackIndex--;
-            currentStackSize--;
+            currentStackSize = maxSize;
             return list.get(currentStackIndex).pop();
         }
 
@@ -59,35 +62,57 @@ public class SetOfStacks {
         return list.get(currentStackIndex).pop();
     }
 
+    public Integer popAt(int index) {
+
+        if (index < 0 || index > currentStackIndex) {
+            throw new RuntimeException();
+        }
+
+        if (index == currentStackIndex) {
+            return pop();
+        }
+
+        var result = list.get(index).pop();
+
+        shiftElements(index);
+        return result;
+    }
+
+    public Integer peek() {
+        return list.get(currentStackIndex).peek();
+    }
+
+
     private void addStackToList() {
         list.add(new Stack<>());
     }
 
+    private void shiftElements(int startIndex) {
+
+        for (var i = startIndex; i <= currentStackIndex - 1; i++) {
+            move(list.get(i + 1), buffer);
+            list.get(i).push(buffer.pop());
+            move(buffer, list.get(i + 1));
+        }
+
+    }
+
+    private void move(Stack<Integer> source, Stack<Integer> dest) {
+        while (!source.isEmpty()) {
+            dest.push(source.pop());
+        }
+    }
 
     public static void main(String[] args) {
 
-        SetOfStacks set = new SetOfStacks(3);
+        SetOfStacks set = new SetOfStacks(4);
 
-        set.push(1);
-        set.push(2);
-        set.push(3);
-        set.push(4);
-        set.push(5);
-        set.push(6);
-        set.push(7);
-        set.push(8);
-        set.push(9);
-        set.push(10);
-        set.push(11);
+        for (int i = 1; i < 20; i++) {
+            set.push(i);
+        }
 
+        set.popAt(1);
         set.pop();
-        set.pop();
-        set.pop();
-        set.pop();
-        set.pop();
-        set.pop();
-
-        System.out.println("bgg");
 
     }
 }
