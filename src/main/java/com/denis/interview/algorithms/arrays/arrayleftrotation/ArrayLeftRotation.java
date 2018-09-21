37e705f231df50544e5e6d6419b5766624d8b947
DiNoWaR <dinowar1@gmail.com>
2018-09-21 23:13:14 +0300

    Adds sub sequence task

--- src/main/java/com/denis/interview/algorithms/arrays/arrayleftrotation/ArrayLeftRotation.java
+++ src/main/java/com/denis/interview/algorithms/arrays/arrayleftrotation/ArrayLeftRotation.java
@@ -36,8 +36,6 @@ public class ArrayLeftRotation {
     public static void main(String[] args) {
 
         var array = new int[]{1, 2, 3, 4, 5};
-
         System.out.println(Arrays.toString(rotateArray(array, 4)));
-
     }
 }
---
+++ src/main/java/com/denis/interview/algorithms/arrays/shortestsubsequence/ShortestSubSequence.java
@@ -0,0 +1,63 @@
+package com.denis.interview.algorithms.arrays.shortestsubsequence;
+
+
+import java.util.*;
+
+/**
+ * You are given two arrays, one shorter (with all distinct elements) and one longer.
+ * Find the shortest subarray in the longer array that contains all the elements in the shorter array.
+ * The items can appear in any order.
+ *
+ * <p>
+ * Integer[] input = {1, 5, 9};
+ * Integer[] source = {7, 5, 9, 0, 2, 1, 3, 5, 7, 9, 1, 1, 5, 8, 8, 9, 7};
+ * <p>
+ * Returns [7,10] (indexes of the shortest subsequence)
+ */
+public class ShortestSubSequence {
+
+
+    public static Integer[] findShortestSubsequence(Integer[] input, Integer[] source) {
+
+        Set<Integer> inputSet = new HashSet<>(Arrays.asList(input));
+        Set<Integer> tempSet = new HashSet<>();
+
+        int left = 0;
+        int right = 0;
+
+        List<Integer[]> sequences = new ArrayList<>();
+
+
+        for (int i = 0; i < source.length; i++) {
+
+            if (inputSet.contains(source[i])) {
+
+                if (tempSet.isEmpty()) {
+                    left = i;
+                }
+
+                tempSet.add(source[i]);
+
+                if (tempSet.equals(inputSet)) {
+                    right = i;
+                    tempSet.clear();
+                    sequences.add(new Integer[]{left, right});
+                }
+            }
+        }
+
+        Comparator<Integer[]> sequenceComparator = Comparator.comparingInt(s -> s[1] - s[0]);
+
+        return sequences.stream().min(sequenceComparator).orElse(new Integer[]{0, 0});
+    }
+
+
+    public static void main(String[] args) {
+
+        Integer[] input = {1, 5, 9};
+        Integer[] source = {7, 5, 9, 0, 2, 1, 3, 5, 7, 9, 1, 1, 5, 8, 8, 9, 7};
+
+        System.out.println(Arrays.toString(findShortestSubsequence(input, source)));
+
+    }
+}
