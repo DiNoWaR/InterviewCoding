80daebdb7adb42801e22a2676e6b02efb76addc5
devasilyev <devasilyev@griddynamics.com>
2018-11-29 16:41:47 +0300

    Adds graph

---
+++ src/main/java/com/denis/interview/algorithms/graphs/implementation/Edge.java
@@ -0,0 +1,28 @@
+package com.denis.interview.algorithms.graphs.implementation;
+
+public class Edge<T> {
+
+    private Node<T> node1;
+    private Node<T> node2;
+    private int weight;
+
+
+    public Edge(Node<T> node1, Node<T> node2, int weight) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.weight = weight;
+    }
+
+
+    public Node<T> fromNode() {
+        return node1;
+    }
+
+    public Node<T> toNode() {
+        return node2;
+    }
+
+    public boolean isBetween(Node<T> node1, Node<T> node2) {
+        return (this.node1 == node1 && this.node2 == node2);
+    }
+}
