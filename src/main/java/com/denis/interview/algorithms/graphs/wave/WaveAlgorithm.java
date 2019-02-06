c364421f83697db212d6164a0579feb3685957f5
DiNoWaR <dinowar1@gmail.com>
2019-02-07 00:50:28 +0300

    Adds Wave algorithm

--- src/main/java/com/denis/interview/algorithms/graphs/wave/WaveAlgorithm.java
+++ src/main/java/com/denis/interview/algorithms/graphs/wave/WaveAlgorithm.java
@@ -4,7 +4,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-
+/**
+ * Implement wave algorithm for path routing
+ */
 public class WaveAlgorithm {
 
 
@@ -13,7 +15,9 @@ public class WaveAlgorithm {
         Stack<MazeNode> path = new Stack<>();
 
         LinkedList<MazeNode> queue = new LinkedList<>();
+
         start.mark = 0;
+        start.visited = true;
         queue.add(start);
 
         if (propagateWave(maze, queue, end)) {
@@ -23,20 +27,43 @@ public class WaveAlgorithm {
         return path;
     }
 
-    private static void recoverPath(MazeNode[][] maze, MazeNode start, MazeNode end, Stack<MazeNode> path) {
+    private static boolean propagateWave(MazeNode[][] maze, LinkedList<MazeNode> queue, MazeNode end) {
 
-        path.push(end);
+        boolean pathExists = false;
 
-        LinkedList<MazeNode> queue = new LinkedList<>();
-        queue.add(end);
-
-        while (true) {
+        while (!queue.isEmpty()) {
 
             MazeNode current = queue.poll();
 
-            if (current == null) {
-                return;
+            if (current.equals(end)) {
+                pathExists = true;
+                break;
+            } else {
+                List<MazeNode> neighbours = MazeUtil.getNeighbours(current, maze);
+
+                for (MazeNode node : neighbours) {
+
+                    if (!node.visited && !node.wall) {
+                        node.mark = current.mark + 1;
+                        node.visited = true;
+                        queue.add(node);
+                    }
+                }
             }
+        }
+        return pathExists;
+    }
+
+    private static void recoverPath(MazeNode[][] maze, MazeNode start, MazeNode end, Stack<MazeNode> path) {
+
+        LinkedList<MazeNode> queue = new LinkedList<>();
+
+        path.push(end);
+        queue.add(end);
+
+        while (!queue.isEmpty()) {
+
+            MazeNode current = queue.poll();
 
             if (current.equals(start)) {
                 break;
@@ -53,62 +80,30 @@ public class WaveAlgorithm {
                 }
             }
         }
-
-    }
-
-    private static boolean propagateWave(MazeNode[][] maze, LinkedList<MazeNode> queue, MazeNode end) {
-
-        boolean pathExists = false;
-
-        int mark = 0;
-
-        while (true) {
-
-            MazeNode current = queue.poll();
-
-            if (current == null) {
-                return false;
-            }
-
-            current.visited = true;
-
-            if (current.equals(end)) {
-                pathExists = true;
-                break;
-            } else {
-                List<MazeNode> neighbours = MazeUtil.getNeighbours(current, maze);
-
-                mark++;
-
-                for (MazeNode node : neighbours) {
-
-                    if (!node.visited && !node.wall) {
-                        node.mark = mark;
-                        queue.add(node);
-                    }
-                }
-            }
-        }
-        return pathExists;
     }
 
 
     public static void main(String[] args) {
 
-        MazeNode[][] maze = MazeUtil.generateMaze(20);
+        int size = 5;
 
-        for (int i = 0; i < 19; i++) {
-            maze[i][3].wall = true;
-        }
+        MazeNode[][] maze = MazeUtil.generateMaze(size);
 
-        Stack<MazeNode> path = wave(maze, maze[0][0], maze[4][4]);
+        MazeUtil.setWall(maze, 0, 1);
+        MazeUtil.setWall(maze, 1, 1);
+        MazeUtil.setWall(maze, 2, 1);
+        MazeUtil.setWall(maze, 3, 1);
+        MazeUtil.setWall(maze, 4, 3);
+        MazeUtil.setWall(maze, 3, 3);
+        MazeUtil.setWall(maze, 2, 3);
+
+
+        Stack<MazeNode> path = wave(maze, maze[0][0], maze[size - 1][size - 1]);
 
         while (!path.isEmpty()) {
 
             MazeNode node = path.pop();
-
             System.out.println("Node X = " + node.x + " Y = " + node.y);
         }
-
     }
 }
