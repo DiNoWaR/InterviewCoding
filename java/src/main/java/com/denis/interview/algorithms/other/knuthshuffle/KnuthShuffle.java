8adc33a4527d5aad442389d1ee46fbcbb8574c06
DiNoWaR <dinowar1@gmail.com>
2019-02-20 23:14:52 +0300

    Adds knuth shuffle

---
+++ java/src/main/java/com/denis/interview/algorithms/other/knuthshuffle/KnuthShuffle.java
@@ -0,0 +1,39 @@
+package com.denis.interview.algorithms.other.knuthshuffle;
+
+/**
+ * Implement the Knuth shuffle for an array
+ */
+public class KnuthShuffle<T> {
+
+
+    public void shuffle(T[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            int currentIndex = (int) (Math.random() * (i + 1));
+            swap(array, currentIndex, i);
+        }
+    }
+
+    private void swap(T[] array, int from, int to) {
+
+        T temp = array[from];
+        array[from] = array[to];
+        array[to] = temp;
+    }
+
+
+    public static void main(String[] args) {
+
+        KnuthShuffle<Integer> knuthShuffle = new KnuthShuffle();
+
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        knuthShuffle.shuffle(array);
+
+        for (Integer item : array) {
+            System.out.print(item + " ");
+        }
+
+    }
+
+}
