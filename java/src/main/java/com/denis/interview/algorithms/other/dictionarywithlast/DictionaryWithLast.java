eb364ac230dc994d3fb2f869371eef882dbfd07d
DiNoWaR <dinowar1@gmail.com>
2019-02-26 12:39:52 +0300

    Adds Dictionary with last

---
+++ java/src/main/java/com/denis/interview/algorithms/other/dictionarywithlast/DictionaryWithLast.java
@@ -0,0 +1,36 @@
+package com.denis.interview.algorithms.other.dictionarywithlast;
+
+/**
+ * Write a new data structure, "Dictionary with Last"
+ * <p>
+ * Methods:
+ * set(key, value) - adds an element to the dictionary
+ * get(key) - returns the element
+ * delete(key) - removes the element
+ * last() - returns the last key that was added or read.
+ * <p>
+ * In case a key was removed, last will return the previous key in order.
+ */
+public class DictionaryWithLast<K, V> {
+
+
+
+
+    public void set(K key, V value) {
+
+    }
+
+    public V get(K key) {
+
+    }
+
+    public void delete(K key) {
+
+    }
+
+    public K last() {
+
+    }
+
+
+}
