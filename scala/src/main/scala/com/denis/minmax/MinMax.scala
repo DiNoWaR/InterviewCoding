4a78108633a09dd59a1d9d2d31d0cf58a7411e65
DiNoWaR <dinowar1@gmail.com>
2019-03-20 02:45:10 +0300

    Adds DecodeWay task

---
+++ scala/src/main/scala/com/denis/minmax/MinMax.scala
@@ -0,0 +1,50 @@
+package com.denis.minmax
+
+import scala.collection.mutable.ArrayBuffer
+
+
+object MinMax {
+
+
+  def getMinMax(size: Int, list: ArrayBuffer[Int]): Int = {
+
+    if (size == 1) {
+      list.max
+    }
+
+    else if (list.size == 1) {
+      list.head
+    }
+
+    else if (size == list.size) {
+      list.min
+    }
+
+    else {
+
+      var max = 0
+
+      var index = 0
+
+      while (index <= list.size - size - 1) {
+
+        val commonMin = list.slice(index + 1, index + size).min
+
+        val left = Math.min(list(index), commonMin)
+        val right = Math.min(list(index + size), commonMin)
+
+        if (left > max) {
+          max = left
+        }
+
+        if (right > max) {
+          max = right
+        }
+
+        index = index + 1
+      }
+      max
+    }
+  }
+
+}
---
+++ scala/src/test/scala/com/denis/minmax/MinMaxTest.scala
@@ -0,0 +1,39 @@
+package com.denis.minmax
+
+import org.scalatest.FunSuite
+import scala.collection.mutable.ArrayBuffer
+
+
+class MinMaxTest extends FunSuite {
+
+  test("1") {
+    val input: ArrayBuffer[Int] = Array(0).to[ArrayBuffer]
+    val result = MinMax.getMinMax(1, input)
+    assert(result == 0)
+  }
+
+  test("2") {
+    val input: ArrayBuffer[Int] = Array(0, 1).to[ArrayBuffer]
+    val result = MinMax.getMinMax(1, input)
+    assert(result == 1)
+  }
+
+  test("3") {
+    val input: ArrayBuffer[Int] = Array(0, 1).to[ArrayBuffer]
+    val result = MinMax.getMinMax(2, input)
+    assert(result == 0)
+  }
+
+  test("4") {
+    val input: ArrayBuffer[Int] = Array(0, 1, 2).to[ArrayBuffer]
+    val result = MinMax.getMinMax(1, input)
+    assert(result == 2)
+  }
+
+  test("5") {
+    val input: ArrayBuffer[Int] = Array(0, 1, 2).to[ArrayBuffer]
+    val result = MinMax.getMinMax(2, input)
+    assert(result == 1)
+  }
+
+}
